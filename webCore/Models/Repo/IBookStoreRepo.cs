c70448bda5ad199245dccb7f89b085c5bdbe34d2
Zayd Elimrani <plutorider47@gmail.com>
2021-06-11 01:14:54 +0100

    Add project files.

---
+++ webCore/Models/Repo/IBookStoreRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webCore.Models.Repo
+{
+    interface IBookStoreRepo<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+
+    }
+}
